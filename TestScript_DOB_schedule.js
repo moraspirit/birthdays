8db837e10690d2725e3ebb2cbaf6061fa9d44534
wink94 <mwindula16@gmail.com>
2016-09-30 21:25:28 +0530

    commit 1-> node scheduler

---
+++ TestScript_DOB_schedule.js
@@ -0,0 +1,65 @@
+
+
+var schedule=require('node-schedule');
+var mysql = require('mysql'); // mysql libaray
+
+/*******schedule time*******/
+
+var rule=new schedule.RecurrenceRule();
+rule.hour=0;
+rule.minute=1;
+rule.second=1;
+
+var task=schedule.scheduleJob(rule,function() {
+
+
+
+    /********create connection**********/
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "msp1"
+    });
+
+    /****check connection**********/
+
+    con.connect(function (err) {
+        if (err) {
+            console.log("connection error");
+            return;
+        }
+        console.log("connected");
+    });
+    /********get system date and month***********/
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    console.log(month + "-" + day);
+
+    /*********querying and getting data***********/
+
+    var qstring = "SELECT * FROM user_details WHERE user_DOB  LIKE '%" + month + "-" + day + "'";
+//var qstring="SELECT * FROM user_details";
+    con.query(qstring, function (err, rows) {
+        if (err)throw err;
+
+
+        
+        for (var i = 0; i < rows.length; i++) {
+            console.log(rows[i]["user_name"] + " : " + rows[i]["user_email"]);
+        }
+
+
+    });
+
+
+    /****terminate connection***********/
+
+    con.end(function (err) {
+        //console.log(err);
+    });
+
+
+});
